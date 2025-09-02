f05a5a77aaee4522e87076cd78fb908c8b9f07ca
ranjith <ranjithp27122003@gmail.com>
2025-09-02 23:23:01 +0530

    added sorting in meditation table

--- src/app/(dashboard)/dashboard/meditation/page.tsx
+++ src/app/(dashboard)/dashboard/meditation/page.tsx
@@ -28,6 +28,18 @@ import { Button } from "@/components/ui/button"
 export default function MeditationTable() {
     const [data, setData] = React.useState([])
     const [loading, setLoading] = React.useState(true)
+    const [page, setPage] = React.useState(1)
+    const [pagination, setPagination] = React.useState<any>({
+        "total": 0,
+        "page": 0,
+        "limit": 10,
+        "totalPages": 0
+    })
+    const [sortValue, setSortValue] = React.useState<any>({
+        field: '',
+        order: 'desc'
+    })
+
     const extendedColumns = React.useMemo(() => [
         ...mediationColumns,
         {
@@ -45,6 +57,21 @@ export default function MeditationTable() {
         getCoreRowModel: getCoreRowModel(),
     })
 
+    const handleSort = (value: string) => {
+        const [field, order] = value.split("-")
+        setSortValue({
+            field,
+            order
+        })
+    }
+
+    const handleSortClear = () => {
+        setSortValue({
+            field: '',
+            order: 'desc'
+        })
+    }
+
     React.useEffect(() => {
         (async () => {
             try {
@@ -52,18 +79,15 @@ export default function MeditationTable() {
                 const res: any = await fetcher(meditationApis.create, {
                     method: 'GET',
                     params: {
-                        order: 'desc'
-                        // order: sortValue.order,
-                        // sort: sortValue.field,
-                        // subscriptionType: subscriptionType,
-                        // signupMethod,
-                        // page,
+                        limit: 10,
+                        order: sortValue.order,
+                        sort: sortValue.field,
+                        page: page,
                     }
                 });
-
-                console.log(res)
                 if (res) {
-                    setData(res)
+                    setData(res.data)
+                    setPagination(res.pagination)
                 }
             } catch (error) {
                 console.error('Fetch error:', error);
@@ -71,13 +95,13 @@ export default function MeditationTable() {
                 setLoading(false)
             }
         })();
-    }, [])
+    }, [page, sortValue.field, sortValue.order])
 
     return (
         <div className="rounded-[20px]  p-6">
             <div className="flex justify-between items-center">
                 <div className="py-4 font-rubik-400 flex md:flex-row flex-col gap-3">
-                    <Select
+                    {/* <Select
                     // value={signupMethod} onValueChange={handleChange}
                     >
                         <SelectTrigger className="w-44 bg-white">
@@ -123,10 +147,10 @@ export default function MeditationTable() {
                                 Clear Selection
                             </div>
                         </SelectContent>
-                    </Select>
+                    </Select> */}
 
                     <Select
-                    // value={sortValue?.field ? `${sortValue.field}-${sortValue.order}` : ""} onValueChange={handleSort}
+                        value={sortValue?.field ? `${sortValue.field}-${sortValue.order}` : ""} onValueChange={handleSort}
                     >
                         <SelectTrigger className="w-44 bg-white">
                             <SelectValue placeholder="Sort By" className="data-[placeholder]:text-black" />
@@ -134,7 +158,7 @@ export default function MeditationTable() {
 
                         <SelectContent>
                             <div className="px-2 py-1">
-                                <p className="text-sm text-gray-500 px-2 font-rubik-400">Created At</p>
+                                <p className="text-sm text-gray-500 px-2 font-rubik-400">Added Date</p>
                                 <SelectItem value="createdAt-asc" className="font-rubik-400 pl-6">
                                     Ascending
                                 </SelectItem>
@@ -143,21 +167,11 @@ export default function MeditationTable() {
                                 </SelectItem>
                             </div>
 
-                            {/* <div className="px-2 py-1">
-                            <p className="text-sm text-gray-500 px-2 font-rubik-400">Last Login</p>
-                            <SelectItem value="lastLogin-asc" className="font-rubik-400 pl-6">
-                                Ascending
-                            </SelectItem>
-                            <SelectItem value="lastLogin-desc" className="font-rubik-400 pl-6">
-                                Descending
-                            </SelectItem>
-                        </div> */}
-
                             <div className="border-t border-gray-200 my-1" />
 
                             <div
                                 className="cursor-pointer px-3 py-2 text-sm text-red-600 hover:bg-red-50 rounded font-rubik-400 select-none"
-                            // onClick={handleSortClear}
+                                onClick={handleSortClear}
                             >
                                 Clear Selection
                             </div>
@@ -207,6 +221,33 @@ export default function MeditationTable() {
                         )}
                     </TableBody>
                 </Table>
+            </div>
+            <div className="flex items-center justify-end space-x-2 py-4">
+                <div className="text-muted-foreground flex-1 text-sm">
+                    {pagination.total === 0
+                        ? '0 of 0'
+                        : `${(pagination.page - 1) * pagination.limit + 1} - ${Math.min(pagination.page * pagination.limit, pagination.total)} of ${pagination.total}`}
+                </div>
+                <div className="space-x-2">
+                    <Button
+                        variant="outline"
+                        className="font-rubik-400 py-3"
+                        size="sm"
+                        onClick={() => setPage(prev => prev - 1)}
+                        disabled={pagination.page <= 1}
+                    >
+                        Previous
+                    </Button>
+                    <Button
+                        variant="outline"
+                        size="sm"
+                        className="font-rubik-400 py-3"
+                        onClick={() => setPage((prev) => prev + 1)}
+                        disabled={pagination.page * pagination.limit >= pagination.total}
+                    >
+                        Next
+                    </Button>
+                </div>
             </div>
         </div >
     )
